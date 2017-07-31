931aeca0cde8ee74877b0d53f0d3134413344c5b
Chase Paules <cpaules@gmail.com>
2017-07-31 21:22:47 +0000

    added some comments in cli.rb

--- lib/movies_near_you/cli.rb
+++ lib/movies_near_you/cli.rb
@@ -3,6 +3,7 @@ class MoviesNearYou::CLI
   def call
     list_movies
     menu
+    bye
     #if input.size != 5 #or contains only numbers
     #  call
     #else
@@ -11,14 +12,25 @@ class MoviesNearYou::CLI
 
   end
 
-  def list_movies #scrapes and displays movies in a numbered list
+  def list_movies #scrapes and displays movies in a numbered list,
+    #movies should be an array of movie objects?
     puts "Enter your 5 digit zip code:"
     zipcode = gets.strip
   end
 
   def menu
-    puts "Enter the number corresponding to which movie you would like to know more about"
-    input = gets.strips
+    puts "Enter the number corresponding to which movie you would like to know more about, or enter exit to exit"
+    #should then ask what info would they like to see. ex imdb rating, genres, summary, showtimes
+    # when displaying showtimes, should display a list of theaters and their showtimes 
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+
+    end
+  end
+
+  def bye
+    puts "Thank you for using Movies Near You!"
   end
 
 end
